428cd68b3f498c569c93eac8af70e95f7e5cb26e
Sheldon Duran <sheldonrubenduran@gmail.com>
2024-11-05 12:15:05 -0800

    Fix server binding to Heroku PORT

--- server.js
+++ server.js
@@ -41,3 +41,8 @@ app.listen(port, () => {
 });
 const cors = require('cors');
 app.use(cors({ origin: 'https://sparklemotion4ever.github.io' }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
