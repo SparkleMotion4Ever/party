5eaee9f61244f5e678415131d8055d46d04b2bde
Sheldon Duran <sheldonrubenduran@gmail.com>
2024-11-05 20:37:17 -0800

    Fix route definition for /api/best-bar

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-
+app.use(cors({ origin: '*' }));
 const app = express();
 
 app.use(cors({ origin: '*' }));
